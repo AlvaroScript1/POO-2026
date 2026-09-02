8b7a171e02734a03d019658062937baf2157ce94
Kevincito <kevin.velasquez2601@alumnos.ubiobio.cl>
2026-09-02 17:01:55 -0400

    Hola, esta es una prueba para ver si funciona el commit en la rama main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     static void main(String[] args) {
         System.out.println("Hello World");
+        // Dejo este comentario para probar el tema del git add . / git commit y git push
     }
 }
