d54a7add8ce74af40175afaa6cacbd7fd73f46a7
Kevincito <kevin.velasquez2601@alumnos.ubiobio.cl>
2026-09-03 10:24:52 -0400

    Agregue un system.out

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Que loco es GitHub");
         // Dejo este comentario para probar el tema del git add . / git commit y git push
     }
 }
