e02c20ccc65e080fd7eddbf0b696044935456608
Kevincito <kevin.velasquez2601@alumnos.ubiobio.cl>
2026-09-06 21:36:31 -0300

    Agregue java.time.* y parte del metodo addMedicion, asi como getLastMedicion, solo que este ultimo no returna null como se estima, mañana corregire el tema del null que generalmente sucede cuando la lista esta vacia que es en este caso, asi que deberia poner un if.

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.time.*;
 public abstract class Sensor {
     private String codigo;
     private String marca;
@@ -29,6 +30,18 @@ public abstract class Sensor {
     }
     public void setEstado(Estado estado){
         this.estado = estado;
+    }
+
+    public boolean addMedicion(LocalDateTime fechaHora, float valor){
+        if(estado.equals(Estado.ACTIVO)){
+            return true;
+        }
+        return false;
+    }
+
+    public Medicion getLastMedicion(){ // En este caso queremos el ultimo objeto medicion de nuestra lista mediciones
+        Medicion lastMedicion = mediciones.getLast(); // Pero si esta vacia deberia retornar null
+        return lastMedicion; // Si tiene objetos entonces retornamos simplemente la ultima medicion
     }
 
 }
