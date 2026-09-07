4e0ae52038a453da58e1fcfa0973995ed65dcb8f
Kevincito <kevin.velasquez2601@alumnos.ubiobio.cl>
2026-09-06 21:08:06 -0300

    Agregue las clases EstacionMetereologica y Medicion con el fin de que no me generara error el codigo debido a que no estaban creadas, luego agregre los atributos correspondientes ademas del arraylist de la clase Medicion, por ultimo finalice el constructor

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
 public abstract class Sensor {
     private String codigo;
     private String marca;
     private String modelo;
     private Estado estado;
+    private ArrayList<Medicion> mediciones;
+    private EstacionMetereologica estacion;
 
-    protected Sensor(String codigo, String modelo, EstacioMetereologica estacion) {
+    protected Sensor(String codigo, String marca, String modelo, EstacionMetereologica estacion) {
         this.codigo = codigo;
         this.modelo = modelo;
         this.estacion = estacion;
+        this.estado = Estado.ACTIVO;
+        this.mediciones = new ArrayList<>();
     }
 
 }
