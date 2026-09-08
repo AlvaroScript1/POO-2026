f62aa3cb24d949ef0ef3237980f72a099cde8789
victor <victor.diaz2501@gmail.ubiobio.cl>
2026-09-08 16:11:33 -0300

    3 metodos creados

---
+++ src/InstitutoMeteorologia.java
@@ -0,0 +1,14 @@
+public class InstitutoMeteorologia {
+
+    public boolean creaRegion (int codigo, String nombre){
+        return false;
+    }
+
+    public boolean creaComuna (int codigo, String nombre, int codigoRegion){
+        return false;
+    }
+
+    public boolean creaEstacion (String cod, String nombre, float lon, float lat, float alt, int codigoRegion, int codComuna){
+        return false;
+    }
+}
