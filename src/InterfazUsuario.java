d566727e51529c5a1ae52fcc431ab29a551fcb90
AlvaroScript1 <alvaro.nunez.rojas123@gmail.com>
2026-09-08 10:57:44 -0300

    esqueleto del main "interfazUsuario" creado.

---
+++ src/InterfazUsuario.java
@@ -0,0 +1,41 @@
+import java.util.*;
+private Scanner sc = new Scanner(System.in);
+
+void main() {
+}
+private void menuPrincipal(){
+
+}
+private void crearRegion(){
+
+}
+private void crearComuna(){
+
+}
+private void crearEstacionMeteorologica(){
+
+}
+private void instalarSensor(){
+
+}
+private void registrarMedicion(){
+
+}
+private void menuListados(){
+
+}
+private void listarRegiones(){
+
+}
+private void listarComunas(){
+
+}
+private void listarEstaciones(){
+
+}
+private void listarSensores(){
+
+}
+private void listarMediciones(){
+
+}
